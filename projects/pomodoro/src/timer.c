37c67328ae9735a88d9d71e5ff027851e1abc336
William Harris <willh.harris@gmail.com>
2024-03-06 22:05:34 -0800

    new project: pmodoro

---
+++ projects/pomodoro/src/timer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+
+// declare time variables
+
+
+// ask user for time or popmdoro loop
+
+
+// run count down time maybe, set brake timer, maybe set timer and brake time
+
+
+
+// maybe exit with a sysem bell
+
+return 0;
+}
